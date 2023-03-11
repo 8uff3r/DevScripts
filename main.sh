46edac581e7e2c5f4532225299013c66be2c4020
8uff3r <8uff3r@gmail.com>
2023-03-11 12:23:48 +0330

    Fix exit bug

--- main.sh
+++ main.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 #
+main() {
 
-ACTIONS=('Install Node Packages' "Install System Pakcages" "Clone configs from git")
-echo $ACTIONS
-ACTION=$(gum choose --cursor-prefix "○ " --selected-prefix "◉ " --unselected-prefix="○ " "${ACTIONS[@]}")
+	trap "{ exit 0; }" SIGINT SIGTERM ERR EXIT
+	ACTIONS=('Install Node Packages' "Install System Pakcages" "Clone configs from git" "exit")
+	ACTION=$(gum choose --cursor-prefix "○ " --selected-prefix "◉ " --unselected-prefix="○ " "${ACTIONS[@]}")
 
-if [[ $ACTION == "Install Node Packages" ]]; then
-	echo "Installing Node Packages"
-	bash ./node-installer.sh
-elif [[ $ACTION == "Install System Pakcages" ]]; then
-	echo "Installing System Pakcages"
-	bash ./sys-installer.sh
-elif [[ $ACTION == "Clone configs from git" ]]; then
-	echo "Cloning configs from git"
-fi
+	if [[ $ACTION == "Install Node Packages" ]]; then
+		echo "Installing Node Packages"
+		bash ./node-installer.sh
+	elif [[ $ACTION == "Install System Pakcages" ]]; then
+		echo "Installing System Pakcages"
+		bash ./sys-installer.sh
+	elif [[ $ACTION == "Clone configs from git" ]]; then
+		echo "Cloning configs from git"
+	elif [[ $ACTION == "exit" ]]; then
+		exit 0
+	fi
+	main
+}
+
+main "$@"
