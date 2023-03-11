73d37ecca8548c4709f9d0ef8cfe14581a609eaa
8uff3r <8uff3r@gmail.com>
2023-03-11 12:32:19 +0330

    fix node installer

--- node-installer.sh
+++ node-installer.sh
@@ -3,9 +3,9 @@
 main() {
 	[[ -x "$(command -v node)" ]] || bash ./sys-installer.sh nodejs
 	PACKAGES="$(cat ./packages/npm/packages)"
-
 	echo "Selct your favourite node packages manager"
-	npm=$(gum choose --cursor-prefix "○ " --selected-prefix "◉ " --no-limit "npm" "pnpm" "yarn")
-	$npm install -g $PACKAGES
+	NPM=$(gum choose --cursor-prefix "○ " --selected-prefix "◉ " "npm" "pnpm" "yarn")
+	SELECTED="$(gum choose --cursor-prefix "○ " --selected-prefix "◉ " --no-limit $PACKAGES)"
+	$NPM install -g $SELECTED
 }
 main "$@"
